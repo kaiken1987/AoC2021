54ec9d76a74b559626a448b2ac547948fd6cd8c3
Ken Aiken <ken@hypack.com>
2021-12-03 13:38:34 -0500

    Day 1

---
+++ Day1/Day1.py
@@ -0,0 +1,32 @@
+
+def part1():
+    f = open("C:\\Code\\GIT_ROOT\\AdventOfCode\\2021\\AoC2021\\Day1\\Input.txt", "r")
+    count = 0
+    prev = -1
+    for x in f:
+        curr = int(x)
+        if (prev > 0) & (curr>prev): 
+            count = count + 1
+        prev = curr
+        
+    print( 'Number incr ' + str( count ) )
+
+
+def part2():
+    f = open("C:\\Code\\GIT_ROOT\\AdventOfCode\\2021\\AoC2021\\Day1\\Input.txt", "r")
+    count = 0
+    prev1 = -1
+    prev2 = -1
+    prev3 = -1
+    for x in f:
+        curr = int(x)
+        if (prev1 > 0) & (curr>prev1): 
+            count = count + 1
+        prev1 = prev2
+        prev2 = prev3
+        prev3 = curr
+        
+    print( 'Number incr ' + str( count ) )
+
+part1()
+part2()
