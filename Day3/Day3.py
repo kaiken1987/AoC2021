bc2c2a0b7d7361f6522113cf33e94ccc59edde45
Ken Aiken <ken@hypack.com>
2021-12-03 15:15:11 -0500

    Day 3 part 2

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,6 +1,9 @@
 from collections import Counter
 
 f = open("Day3\\Input.txt", "r")
+vals = []
+for s in f:
+    vals.append(s.strip())
 
 def bi2int( s = "11111" ):
     val = 0
@@ -23,10 +26,9 @@ def invertArr( arr ):
     for i in range(len(arr)):
         arr[i] = -arr[i]
 
-
 def part1():
     commons = [0,0,0,0,0,0,0,0,0,0,0,0,0]
-    for s in f:
+    for s in vals:
         for i in range(len(s)):
             c = s[i]
             if c == '0':
@@ -42,7 +44,36 @@ def part1():
     
 
     print( "Power Consumption: " + str(gamma*epsilon) )
-def part2():
-    print( "Part 2")
+def part2(vals, least = False):
+    l = len( vals[0])
+    for i in range(l):
+        common = 0
+        for s in vals:
+            c = s[i]
+            if c == '0':
+                common -= 1
+            if c == '1':
+                common += 1
+        if least:
+            common *= -1
+            if common == 0:
+                common -= 1
+        newvals = []
+        for j in range(len(vals)):
+            s = vals[j]
+            c = s[i]
+            if ((c == '0') & (common<0) ) | ((c == '1') & (common>=0)):
+                newvals.append(s)
+        vals = newvals
+        if len(newvals) <= 1:
+            v = bi2int(vals[0])
+            print("last val: " + str(v))
+            return v
+
 
 part1()
+o2 = vals
+co2 = vals
+o2 = part2(o2)
+co2 = part2(co2, True)
+print( "Part 2: " + str(o2*co2))
