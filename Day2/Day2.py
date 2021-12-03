92116d9e20cc32d7c2f401a4c45e296b8da33d86
Ken Aiken <ken@hypack.com>
2021-12-03 14:02:59 -0500

    Day 2 part 2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,17 +1,33 @@
 f = open("Day2\\Input.txt", "r")
 
-dist = 0
-depth = 0
+def part1():
+    dist = 0
+    depth = 0
+    for x in f:
+        cmd, val = x.split(" ", 1)
+        val = int(val)
+        if cmd == "forward":
+            dist = dist + val
+        if cmd == "down":
+            depth = depth + int(val)
+        if cmd == "up":
+            depth = depth - int(val)
+    print( "Final Position " + str(dist) + ", " + str(depth) + " x*y: " + str(dist*depth) )
+    
+def part2():
+    dist = 0
+    depth = 0
+    aim = 0
+    for x in f:
+        cmd, val = x.split(" ", 1)
+        val = int(val)
+        if cmd == "forward":
+            dist = dist + val
+            depth = depth + (aim*val)
+        if cmd == "down":
+            aim = aim + val
+        if cmd == "up":
+            aim = aim - val
+    print( "Final Position " + str(dist) + ", " + str(depth) + " x*y: " + str(dist*depth) )
 
-for x in f:
-    cmd, val = x.split(" ", 1)
-    val = int(val)
-    if cmd == "forward":
-        dist = dist + val
-    if cmd == "down":
-        depth = depth + int(val)
-    if cmd == "up":
-        depth = depth - int(val)
-    
-print( "Final Position " + str(dist) + ", " + str(depth) + " x*y: " + str(dist*depth) )
-    
+part2() 
