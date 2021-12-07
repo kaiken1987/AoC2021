300a3e2fec623224e00f4ef0a411f01f78ccfa5c
Ken Aiken <ken@hypack.com>
2021-12-07 17:47:54 -0500

    Day 7 Part 1 & 2

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -1,9 +1,38 @@
-f = open("Day7\\TestInput.txt", "r")
+f = open("Input.txt", "r")
+
+positions = [int(x) for x in f.readline().split(",")]
+
+maxPos = 0
+for p in positions:
+   if p>maxPos:
+      maxPos = p
 
 def part1():
-    print( "Part 1")
-def part2():
-    print( "Part 2")
+   print( "Part 1")
+   bestp = 0
+   bestdis = maxPos*len(positions)
+   for x in range(maxPos):
+      dist = 0
+      for p in positions:
+         dist += abs(p-x)
+      if(dist<bestdis):
+         bestdis = dist
+         bestp = x         
+   print( "Best Pos: " + str( bestp ) + " Fuel: " + str(bestdis) )
 
-part1() 
+def part2():
+   print( "Part 2")
+   bestp = 0
+   bestdis = maxPos*maxPos*len(positions)
+   for x in range(maxPos):
+      dist = 0
+      for p in positions:
+         d = abs(p-x)
+         d = d*(d+1)/2
+         dist += d
+      if(dist<bestdis):
+         bestdis = dist
+         bestp = x
+   print( "Best Pos: " + str( bestp ) + " Fuel: " + str(bestdis) )
+#part1() 
 part2() 
