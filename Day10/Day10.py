d326a9df0af306ce4a7744c751f16931a887932f
Ken Aiken <ken@hypack.com>
2021-12-10 09:55:07 -0500

    Day 10 part 2 and added README

--- Day10/Day10.py
+++ Day10/Day10.py
@@ -1,28 +1,44 @@
 f = open("Day10\\Input.txt", "r")
-
-def part1():
-    print( "Part 1")
-    stack = []#{'(':0,'[':0,'{':0,'<':0}
-    open = {'(':')','[':']','{':'}','<':'>'}
-    #close = {')':'(',']':'[','}':'{','>':'<'}
-    errors = {')':0,']':0,'}':0,'>':0}
+valid = []
+def day10():
+    #part 1 stuff
+    open = {'(':')','[':']','{':'}','<':'>'} #convert open chars to close chars
+    errors = {')':0,']':0,'}':0,'>':0} # count of illegal char for answer
+    #part 2
+    scoring = {')':1,']':2,'}':3,'>':4}
+    autoScores = [] #list of scores for auto complete
     for line in f:
         line = line.strip()
+        error = False
+        stack = [] #unclosed chunk stack
+        #part1 find mis matched closes
         for c in line:
             if c in ['(','[','{','<']:
-                stack.append(c)
-            else:
-                #c = close[c]
+                stack.append(c) #open chunk
+            else: #close chunk
                 o = open[stack.pop()]
-                if o != c:                    
+                if o != c:    #found c expect o to close chunk
                     errors[c] += 1
+                    error = True #error found skip line for part 2
                     break
+        if  error:
+            continue
+        #part2 auto complete line
+        score = 0
+        while len(stack)>0:
+            score *= 5
+            c = open[stack.pop()]
+            score += scoring[c]
+        autoScores.append(score)
+    print( "Part 1")
     print(errors)
     sum = errors[')']*3+errors[']']*57+errors['}']*1197+errors['>']*25137
     print( f"Anser: {sum}")
 
-def part2():
     print( "Part 2")
+    autoScores.sort()
+    mid = int(len(autoScores)/2)
+    mid = autoScores[mid]
+    print( f"Anser: {mid}")
 
-part1() 
-part2() 
+day10() 
