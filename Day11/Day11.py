795b20d17eefca504660f8215f124a1d8f47ec97
Ken Aiken <ken@hypack.com>
2021-12-11 10:14:33 -0500

    Day 11 Part 1

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -1,7 +1,46 @@
-f = open("Day11\\TestInput.txt", "r")
+f = open("Day11\\Input.txt", "r")
+
+#load data
+octs = []
+for l in f:
+    l = l.strip()
+    row = [int(x) for x in l]
+    octs.append(row)
+
+def incOct(y,x):
+    if x<0 or y<0:
+        return
+    elif y>=len(octs) or x>=len(octs[y]):
+        return
+    elif octs[y][x]<=9: #only flash if haven't flashed already
+        octs[y][x] += 1     
+        if( octs[y][x]>9 ):
+            for x1 in range(x-1,x+2): 
+                incOct(y-1, x1)#above
+                incOct(y+1, x1)#below
+            incOct(y, x-1)#left
+            incOct(y, x+1)#right
+
+def step():
+    flashes = 0
+    for y in range(len(octs)):
+        for x in range(len(octs[y])):
+            incOct(y,x)
+    #reset if flashed
+    for y in range(len(octs)):
+        for x in range(len(octs[y])):
+            if octs[y][x]>9 :
+                octs[y][x] = 0
+                flashes += 1
+    return flashes 
+
 
 def part1():
     print( "Part 1")
+    flashed = 0
+    for i in range(100):
+        flashed += step()
+    print(f"Flashes: {flashed}")
 def part2():
     print( "Part 2")
 
