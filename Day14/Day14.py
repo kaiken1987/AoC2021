c0727dbb72b4ccb7f0f1d5645e5644f47cbe335a
Ken Aiken <ken@hypack.com>
2021-12-14 15:32:20 -0500

    Day 14 working quickly

--- Day14/Day14.py
+++ Day14/Day14.py
@@ -1,27 +1,19 @@
 import math
 template = []
 inserts = {}
-elements = {}
-
-def tobitFlags(str):
-    result = 0
-    for x in str:
-        result += (1<<(ord(x)-ord('A')))
-    return str
 
 def loadInput():
     global template
-    f = open("Day14\\TestInput.txt", "r")
+    f = open("Day14\\Input.txt", "r")
     input = f.readline().strip()
     template = []
     for x in input:
-        template.append(tobitFlags(x))
+        template.append(x)
     f.readline()
     for x in f:
         a,b = x.strip().split(" -> ",2)
-        a = tobitFlags(a)
-        b = tobitFlags(b)
         inserts[a] = b
+
 def iterate(input):
     result = []
     for i in range(len(input)-1):
@@ -35,22 +27,8 @@ def iterate(input):
     result += input[-1]
     return result 
 
-def process( a, b, maxDep):
-    global elements
-    global inserts
-    ab = a+b
-    c = inserts[ab]
-    if c not in elements:
-        elements[c] = 1
-    else:
-        elements[c] += 1
-    if maxDep>1:
-        process(a,c, maxDep-1)
-        process(c,b, maxDep-1)
-
 def part1():
     global template
-    global elements
     loadInput()
     for i in range(10):
         template = iterate(template)
@@ -72,30 +50,48 @@ def part1():
             max = x
     print( f"Part 1: {max-min}")
 
+def incPair( pairs, ab, cnt=1):
+    if ab in pairs:
+        pairs[ab] += cnt
+    else:
+        pairs[ab] = cnt
+
 def part2():
     global template
-    global elements
     global inserts
     maxDepth = 40
+    pairCnt = {}
     elements = {}
     loadInput()
-    #for x in range(26):
-    #    x = chr(x+ord('A'))
-    #    elements[x] = 0
-    for c in template:
-        elements[c] = 1
     for i in range(len(template)-1):
-        process(template[i], template[i+1], maxDepth)
+        ab = template[i] + template[i+1]
+        incPair( pairCnt, ab)
+    for i in range(maxDepth):
+        newPairs = {}
+        for x in pairCnt:
+            a,b = x
+            c = inserts[x]
+            cnt = pairCnt[x]
+            incPair(newPairs,a+c, cnt)
+            incPair(newPairs,c+b, cnt)
+        pairCnt = newPairs
+    for x in pairCnt:
+        cnt = pairCnt[x]
+        a,b = x
+        if a in elements:
+            elements[a] += cnt
+        else:
+            elements[a] = cnt
+    elements[template[-1]] += 1
+
     min = 2e30
     max = 0
-    counts = {}
-    for k in elements:
-        x = elements[k]
-        if x<min and x>0:
+    for x in elements:
+        x = elements[x]
+        if x<min:
             min = x
         if x>max:
             max = x
-
     print( f"Part 2: {max-min}")
 
 part1() 
