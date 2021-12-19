0a9cdb2d5996a511b54d23a04106bfc1e36b3729
Ken Aiken <ken@hypack.com>
2021-12-19 16:16:24 -0500

    Day15 Work for part 2 and improving priority queue

--- Day15/Day15.py
+++ Day15/Day15.py
@@ -1,7 +1,6 @@
 import sys
 
 nodes = []
-graph = {}
 class node:
    value = -1
    visited = False
@@ -15,31 +14,42 @@ def getNeighbors(x,y,input):
    result = []
    if y>1:
       result.append(input[y-1][x])
-      #if x>1:
-      #   result.append(input[y-1][x-1])
-      #if x+1<len(input[y]):
-      #   result.append(input[y-1][x+1])
    if y+1<len(input):
       result.append(input[y+1][x])
-      #if x>1:
-      #   result.append(input[y+1][x-1])
-      #if x+1<len(input[y]):
-      #   result.append(input[y+1][x+1])
    if x>1:
       result.append(input[y][x-1])
    if x+1<len(input[y]):
       result.append(input[y][x+1])
    return result
 
-def loadFile():
+def loadFile(part2 = False):
    f = open("Day15\\Input.txt", "r")
+   nodes.clear()
    for x in f:
       x = x.strip()
       row = []
       for y in x:
          row.append(node(int(y)))
+      if part2:
+         wid = len(row)
+         for i in range(1,5):
+            for j in range(wid):
+               v = row[j].value+i
+               if v > 9: 
+                  v-=9
+               row.append(node(v))
       nodes.append(row)
-
+   if part2:
+      hei = len(nodes)
+      for i in range(1,5):
+         for j in range(hei):
+            row = []
+            for n in nodes[j]:
+               v = n.value+i
+               if v > 9: 
+                  v-=9
+               row.append(node(v))
+            nodes.append(row)
    for y in range(len(nodes)):
       for x in range(len(nodes[y])):
          nodes[y][x].neighbors = getNeighbors(x,y,nodes)
@@ -48,20 +58,25 @@ class priorityQueue:
    queue = []
    def updateNode(self,node):
       idx = node.idx
-      if(idx <0):
+      if(idx <1):
          return
-      while( idx>0 and self.queue[idx].dist>self.queue[idx-1].dist):
-         temp = self.queue[idx]
-         self.queue[idx] = self.queue[idx-1]
-         self.queue[idx-1] = temp
-         self.queue[idx-1].idx = idx-1
-         self.queue[idx].idx = idx
+      while( idx>0 and self[idx].dist<self[idx-1].dist):
+         temp = self[idx]
+         self[idx] = self[idx-1]
+         self[idx-1] = temp
+         self[idx-1].idx = idx-1
+         self[idx].idx = idx
          idx -= 1
+   def __getitem__(self,key):
+      return self.queue[key]
+   def __setitem__(self,key,value):
+      self.queue[key] = value
 
    def popfront(self):
-      result = self.queue[0]
+      result = self[0]
       for i in range(1,len(self.queue)):
-         self.queue[i-1] = self.queue[i]
+         self[i].idx -= 1
+         self[i-1] = self[i]
       self.queue.pop()
       result.idx = -1
       return result
@@ -77,7 +92,7 @@ def Dijkstra():
    source = 0
    for y in range(len(nodes)):
       for x in range(len(nodes[y])):
-         Q.queue.append(nodes[y][x])
+         Q.addNode(nodes[y][x])
    target = Q.queue[-1]
    Q.queue[0].dist = 0
    curr = 0
@@ -102,7 +117,9 @@ def part1():
     print( f"Part 1: Risk:{risk}")
 
 def part2():
-    print( "Part 2")
+    loadFile(True)
+    risk = Dijkstra()
+    print( f"Part 2: Risk:{risk}")
 
 part1() 
-part2() 
+#part2() 
